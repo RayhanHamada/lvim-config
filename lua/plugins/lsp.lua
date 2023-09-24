f72a6697995f211f66b1bbcf02766c1ec5afe989
Muhammad Rayhan Hamada Budiman <rayhanhamada1999@gmail.com>
2023-09-24 13:17:41 +0700

    feat: add lsp plugins

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,27 @@
+return {
+
+  { import = "lazyvim.plugins.extras.lang.typescript" },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "bash",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "query",
+        "regex",
+        "svelte",
+        "tsx",
+        "typescript",
+        "vim",
+        "yaml",
+      },
+    },
+  },
+}
