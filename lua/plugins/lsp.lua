eb033622b368a38101b45702d27f1c62a8a44ac8
Muhammad Rayhan Hamada Budiman <rayhanhamada1999@gmail.com>
2023-10-14 19:30:40 +0700

    feat: add lsp support

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "css-lsp",
+        "html-lsp",
+        "tailwindcss-language-server",
+        "typescript-language-server",
+        "svelte-language-server",
+        "stylua",
+        "shellcheck",
+        "shfmt",
+        "flake8",
+        "black",
+        "gopls",
+        "delve",
+        "gotests",
+        "golines",
+        "rust-analyzer",
+        "rustfmt",
+        "sqlfluff",
+      },
+    },
+  },
+}
